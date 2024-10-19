d2ea4d44fdcb747b057ec5d010f0942eff925c93
Dejvexik <142047190+dejvexik@users.noreply.github.com>
2024-10-19 10:14:31 +0200

    steal_config.lua

--- configs/steal_config.lua
+++ configs/steal_config.lua
@@ -1,4 +1,4 @@
-wx = {}
-wx.TargetNotification = false -- zobrazit notifikaci druhemu hraci, ze je prohledavan
-wx.Progress = true -- progress bar pri prohledavani
-wx.ProgressLength = 5000 -- trvani progressbaru
+dx = {}
+dx.ProgressLength = 5000
+dx.TargetNotification = false
+dx.Progress = true
