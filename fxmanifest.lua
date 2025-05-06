2c727884453cdea96f93f47cc75be62f355e2db7
Dejvexik <142047190+dejvexik@users.noreply.github.com>
2025-05-06 06:26:51 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ shared_script '@es_extended/imports.lua'
 fx_version 'cerulean'
 game 'gta5'
 version '1.0.0'
-author 'DX (Dejvex1)'
+author 'DX Scripts by Dejvex'
 
 client_scripts {
     'client/*.lua',
